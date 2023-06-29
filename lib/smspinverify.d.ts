307e569eac0f1ed257251a33840d9b782dc11680
pyrosec <inquiries@pyrosec.gg>
2023-06-29 09:54:18 -0500

    update

--- lib/smspinverify.d.ts
+++ lib/smspinverify.d.ts
@@ -1,14 +1,16 @@
 export declare class SmsPinVerifyClient {
     static BASE_URL: string;
     apiKey: string;
+    last: any;
     constructor(o: any);
     toObject(): {
+        last: any;
         apiKey: string;
     };
     toJSON(): string;
     static fromObject(o: any): SmsPinVerifyClient;
     static fromJSON(s: any): SmsPinVerifyClient;
-    _call(pathname: any, data?: {}, headers?: {}): Promise<any>;
+    _call(pathname: any, data?: {}, headers?: {}): Promise<string>;
     checkBalance(): Promise<any>;
     checkRates({ country }: {
         country: any;
@@ -18,9 +20,9 @@ export declare class SmsPinVerifyClient {
         number: any;
         country: any;
         app: any;
-    }): Promise<any>;
+    }): Promise<string>;
     getNumber({ app, country }: {
         app: any;
         country: any;
-    }): Promise<any>;
+    }): Promise<string>;
 }
--- src.ts/smspinverify.ts
+++ src.ts/smspinverify.ts
@@ -2,14 +2,24 @@ import fetch from "node-fetch";
 import qs from 'querystring';
 import { unzip } from "gzip-js";
 
+interface Item {
+  number: string;
+  app: string;
+  country: string;
+}
+
 export class SmsPinVerifyClient {
   static BASE_URL = "https://api.smspinverify.com/user";
   public apiKey: string;
+  public last: any;
   constructor(o) {
     this.apiKey = o.apiKey;
+    this.last = o.last;
+
   }
   toObject() {
     return {
+      last: this.last,
       apiKey: this.apiKey
     };
   }
@@ -48,6 +58,9 @@ export class SmsPinVerifyClient {
     country,
     app
   }) {
+    if (!number) number = (this.last || {}).number;
+    if (!country) country = (this.last || {}).country;
+    if (!app) app = (this.last || {}).app;
     const response = await this._call('/get_sms.php', {
       country,
       number,
